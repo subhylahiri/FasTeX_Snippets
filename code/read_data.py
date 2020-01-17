c5bfb0b23539be38244ebc660b62d69324e90e29
Subhaneil Lahiri <subhylahiri@gmail.com>
2020-01-16 22:39:44 -0800

    writing data json

--- code/read_data.py
+++ code/read_data.py
@@ -2,26 +2,42 @@
 """
 import io
 import re
+import json
 import typing as ty
 
-Snippet = ty.Union[str, ty.List[str]]
+Body = ty.Union[str, ty.List[str]]
+Snippet = ty.Dict[str, Body]
 
 TEMPLATE_PRE = (
     r"Assign\('FasTeX','(\S*)'\);" +
     r"Exe\('%b\\Macros\\Active Strings\\FasTeX\\FasTeX_Templates.edt'\);")
 INSERT_PRE = r"^BeginGroup;Backspace\(\d+\);Ins\([`'](.*)[`']\);"
+LINE_UP = r"LineUp\((\d)\);TrackCaret;"
+BACK_ONE = "CharLeft;"
+BACK_SOME = r"CharLeft\((\d)\);"
+END_GROUP = "EndGroup;$"
 
 TRIGGER = re.compile(r'^STRING="(\S*)  "$')
 MACRO = re.compile(r'^  MACRO="\[(.*)\]"$')
-TEMPLATE_ONE = re.compile(TEMPLATE_PRE + r"LineUp\((\d)\);TrackCaret;$")
+TEMPLATE_ONE = re.compile(TEMPLATE_PRE + LINE_UP + "$")
+TEMPLATE_ONES = re.compile(TEMPLATE_PRE + LINE_UP + BACK_ONE + "$")
 TEMPLATE_ZERO = re.compile(TEMPLATE_PRE + "$")
-INSERT_TWO = re.compile(INSERT_PRE + r"CharLeft;CharLeft\((\d)\);EndGroup;$")
-INSERT_ONE = re.compile(INSERT_PRE + "CharLeft;EndGroup;$")
-INSERT_ONES = re.compile(INSERT_PRE + r"CharLeft\((\d)\);EndGroup;$")
-INSERT_ZERO = re.compile(INSERT_PRE + "EndGroup;$")
+INSERT_TWO = re.compile(INSERT_PRE + BACK_ONE + BACK_SOME + END_GROUP)
+INSERT_ONE = re.compile(INSERT_PRE + BACK_ONE + END_GROUP)
+INSERT_ONES = re.compile(INSERT_PRE + BACK_SOME + END_GROUP)
+INSERT_ZERO = re.compile(INSERT_PRE + END_GROUP)
 
-TEMPLATES = (TEMPLATE_ZERO, TEMPLATE_ONE)
+TEMPLATES = (TEMPLATE_ZERO, TEMPLATE_ONES, TEMPLATE_ONE)
 INSERTS = (INSERT_ZERO, INSERT_ONE, INSERT_ONES, INSERT_TWO)
+
+
+def escape_body(body: str) -> str:
+    """Escape special characters in snippets
+    """
+    if isinstance(body, list):
+        return [escape_body(txt) for txt in body]
+    return body.replace('$', '\\$')
+
 
 def read_dat(file_name: str):
     """Read templates dat file
@@ -40,7 +56,7 @@ def get_template(text: ty.List[str], snip: str) -> ty.List[str]:
     except ValueError:
         raise
     else:
-        return [x[:-1].replace('$', r'\$') for x in text[start:stop]]
+        return [escape_body(x[:-1]) for x in text[start:stop]]
 
 
 def next_ini_line(file: io.TextIOBase, check: re.Pattern) -> re.Match:
@@ -77,7 +93,8 @@ def get_ini_macro(file: io.TextIOBase) -> str:
 
 def get_macro_matches(macro: str,
                       patterns: ty.Sequence[re.Pattern]) -> (re.Match, int):
-    """Get match text from macro"""
+    """Get match object from macro
+    """
     for index, pattern in enumerate(patterns):
         match = pattern.match(macro)
         if match:
@@ -86,26 +103,31 @@ def get_macro_matches(macro: str,
 
 
 def get_macro_template(macro: str, dat_text: ty.List[str]) -> ty.List[str]:
-    """Get template text from macro"""
-    match, _ = get_macro_matches(macro, TEMPLATES)
+    """Get template text from macro
+    """
+    match, index = get_macro_matches(macro, TEMPLATES)
     if match is None:
         return None
     groups = match.groups()
     trigger = groups[0]
     body = get_template(dat_text, trigger)
-    if len(groups) == 2:
+    if index == 2:
         line_num = int(groups[1])
         body[-line_num] += "$1"
+    if index == 1:
+        line_num = int(groups[1])
+        body[-line_num] = body[-line_num][:-1] + "$1" + body[-line_num][-1:]
     return body
 
 
 def get_macro_insert(macro: str) -> str:
-    """Get template text from macro"""
+    """Get inserted text from macro
+    """
     match, index = get_macro_matches(macro, INSERTS)
     if match is None:
         return None
     groups = match.groups()
-    body = groups[0].replace('$', r'\$')
+    body = escape_body(groups[0])
     if index == 0:
         return body
     if index == 1:
@@ -114,11 +136,12 @@ def get_macro_insert(macro: str) -> str:
     if index == 2:
         return body[:-char_num] + "$1" + body[-char_num:]
     body = body[:-char_num-1] + "$1" + body[-char_num-1:]
+    # second tab-stop was a winedt bullet - remove
     return body[:-2] + "$2" + body[-1:]
 
 
 def next_ini_entry(file: io.TextIOBase,
-                   dat_text: ty.List[str]) -> ty.Dict[str, Snippet]:
+                   dat_text: ty.List[str]) -> ty.Dict[str, Body]:
     """Find next entry in ini file
     """
     trigger = get_ini_trigger(file)
@@ -133,24 +156,32 @@ def next_ini_entry(file: io.TextIOBase,
         description = body
     if body is None:
         return None
-    snippet = {'prefix': trigger, 'body': body, 'description': description}
-    return {trigger: snippet}
+    return {'prefix': trigger, 'body': body, 'description': description}
 
 
-def process_ini(ini_file: str, dat_file: str) -> ty.List[ty.Dict[str, Snippet]]:
-    """Read all winedt data"""
+def process_ini(ini_file: str, dat_file: str) -> ty.Dict[str, Snippet]:
+    """Read all winedt data
+    """
     dat_text = read_dat(dat_file)
-    snippets = []
+    snippets = {}
     with open(ini_file, mode='r') as file:
         snip = next_ini_entry(file, dat_text)
         while snip:
-            snippets.append(snip)
+            snippets[snip['prefix']] = snip
             snip = next_ini_entry(file, dat_text)
     return snippets
+
+
+def write_json(file_name: str, snippets: ty.Dict[str, Snippet]):
+    """Write imported snippet data to json file
+    """
+    with open(file_name, 'w') as file:
+        json.dump(snippets, file, indent=4)
 
 
 if __name__ == "__main__":
     snips = process_ini('data/ActiveStrings-FasTeX.ini',
                         'data/FasTeX_Templates.edt.dat')
-    for snip in snips[780:790]:
-        print(snip)
+    write_json('data/data.json', snips)
+    for pref in ['txpf', 'dccd', 'eqtxt', 'xa', 'txt']:
+        print(snips[pref])
